047c6481aebf72e1204a7c6ba8241f4296feb8ad
silvestrevivo <silvestrevivomillan@gmail.com>
2022-06-19 00:10:26 +0200

    register book, first approach

--- programs/solana-svelte-books/src/lib.rs
+++ programs/solana-svelte-books/src/lib.rs
@@ -1,15 +1,30 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("FB5U8m8DBbYBnF6oqzgM7tc2YKpgNicwZvWfZSdKoV7d");
 
 #[program]
 pub mod solana_svelte_books {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create_entry(ctx: Context<CreateEntry>, title: String, author: String) -> Result<()> {
+        let book_account = &mut ctx.accounts.book_account;
+        book_account.title = title;
+        book_account.author = author;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct CreateEntry<'info> {
+    #[account(init, payer = payer, space = 100)]
+    pub book_account: Account<'info, BookAccount>,
+    #[account[mut]]
+    pub payer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct BookAccount {
+    pub title: String,
+    pub author: String,
+}
--- tests/solana-svelte-books.ts
+++ tests/solana-svelte-books.ts
@@ -1,16 +1,26 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
+import { web3 } from '@project-serum/anchor';
 import { SolanaSvelteBooks } from "../target/types/solana_svelte_books";
+const { SystemProgram } = web3;
 
 describe("solana-svelte-books", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env()
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.SolanaSvelteBooks as Program<SolanaSvelteBooks>;
 
+  const bookAccount = web3.Keypair.generate();
+
   it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+
+    await program.methods.createEntry("book title", "book author").accounts({
+      bookAccount: bookAccount.publicKey,
+      payer: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId
+    }).signers([bookAccount]).rpc();
+
+    const account = await program.account.bookAccount.fetch(bookAccount.publicKey.toString());
+    console.log('account: ', account);
   });
 });
