cb35845956bc5765a9d9e40c500f4f5b8235282d
silvestrevivo <silvestrevivomillan@gmail.com>
2022-07-25 09:54:34 +0200

    trying to filter with dynamic strings

--- programs/solana-svelte-books/src/lib.rs
+++ programs/solana-svelte-books/src/lib.rs
@@ -6,25 +6,48 @@ declare_id!("FB5U8m8DBbYBnF6oqzgM7tc2YKpgNicwZvWfZSdKoV7d");
 pub mod solana_svelte_books {
     use super::*;
 
-    pub fn create_entry(ctx: Context<CreateEntry>, title: String, author: String) -> Result<()> {
-        let book_account = &mut ctx.accounts.book_account;
-        book_account.title = title;
-        book_account.author = author;
+    pub fn add_entry(ctx: Context<AddEntry>, title: String, author: String) -> Result<()> {
+        let entry_book = &mut ctx.accounts.entry_book;
+        entry_book.title = title;
+        entry_book.author = author;
+        Ok(())
+    }
+
+    pub fn modify_author(ctx: Context<ModifyAuthor>, author: String) -> Result<()> {
+        let entry_book = &mut ctx.accounts.entry_book;
+        entry_book.author = author;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct CreateEntry<'info> {
-    #[account(init, payer = payer, space = 100)]
-    pub book_account: Account<'info, BookAccount>,
-    #[account[mut]]
-    pub payer: Signer<'info>,
-    pub system_program: Program<'info, System>,
+pub struct AddEntry<'info> {
+    #[account(init, payer = user, space = EntryBook::LEN)]
+    pub entry_book: Account<'info, EntryBook>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>
 }
 
+#[derive(Accounts)]
+pub struct ModifyAuthor<'info> {
+    #[account(mut)]
+    pub entry_book: Account<'info, EntryBook>,
+}
+
+
 #[account]
-pub struct BookAccount {
-    pub title: String,
-    pub author: String,
+#[derive(Default)]
+pub struct EntryBook {
+    title: String,
+    author: String
+}
+
+
+const DISCRIMINATOR_LENGTH: usize = 8;
+const STRING_LENGTH_PREFIX: usize = 4;
+const MAX_STRING_LENGTH: usize = 100 * 4;
+
+impl EntryBook {
+    const LEN: usize =DISCRIMINATOR_LENGTH + (STRING_LENGTH_PREFIX  + MAX_STRING_LENGTH) * 2;
 }
--- tests/solana-svelte-books.ts
+++ tests/solana-svelte-books.ts
@@ -3,24 +3,82 @@ import { Program } from "@project-serum/anchor";
 import { web3 } from '@project-serum/anchor';
 import { SolanaSvelteBooks } from "../target/types/solana_svelte_books";
 const { SystemProgram } = web3;
+import bs58 from 'bs58';
+import { sha256 } from 'js-sha256';
 
 describe("solana-svelte-books", () => {
-  const provider = anchor.AnchorProvider.env()
+  // Configure the client to use the local cluster.
+  const provider = anchor.AnchorProvider.env();
   anchor.setProvider(provider);
 
   const program = anchor.workspace.SolanaSvelteBooks as Program<SolanaSvelteBooks>;
+  const wallet = provider.wallet.publicKey;
 
-  const bookAccount = web3.Keypair.generate();
 
-  it("Is initialized!", async () => {
+  it("Creates entry!", async () => {
 
-    await program.methods.createEntry("book title", "book author").accounts({
-      bookAccount: bookAccount.publicKey,
-      payer: provider.wallet.publicKey,
+    const entryBookAccount = anchor.web3.Keypair.generate();
+    await program.methods.addEntry('Cien años de soledad', 'Ernesto Sabato')
+    .accounts({
+      entryBook: entryBookAccount.publicKey,
+      user: wallet,
       systemProgram: SystemProgram.programId
-    }).signers([bookAccount]).rpc();
+    }).signers([entryBookAccount]).rpc();
 
-    const account = await program.account.bookAccount.fetch(bookAccount.publicKey.toString());
+
+    const account = await program.account.entryBook.fetch(entryBookAccount.publicKey);
     console.log('account: ', account);
   });
+
+  it("Creates second entry!", async () => {
+    const entryBookAccount = anchor.web3.Keypair.generate();
+    await program.methods.addEntry('El tunel', 'Ernesto Sabato')
+    .accounts({
+      entryBook: entryBookAccount.publicKey,
+      user: wallet,
+      systemProgram: SystemProgram.programId
+    }).signers([entryBookAccount]).rpc();
+
+
+    const account = await program.account.entryBook.fetch(entryBookAccount.publicKey);
+    console.log('account: ', account);
+  });
+
+  it('fetches all the entry_book accounts and modify first book', async () => {
+
+    const allEntries = await program.account.entryBook.all();
+    console.log('allEntries: ', allEntries);
+
+    const [firstElementPublicKey] = allEntries.map(i => i.publicKey);
+
+    // modify_author
+    await program.methods.modifyAuthor('Garcia Marquez')
+    .accounts({
+      entryBook: firstElementPublicKey,
+    }).rpc();
+
+    const allEntriesAgain = await program.account.entryBook.all();
+    console.log('allEntriesAgain: ', allEntriesAgain);
+  });
+
+  it('fetches all the books written by Garcia Marquez', async () => {
+    const accountDiscriminator = Buffer.from(sha256.digest('account:EntryBook')).slice(0, 8);
+
+    const filteringBooks = await program.account.entryBook.all([
+      {
+          memcmp: {
+              // offset: 8 + // Discriminator.
+              //     4 +  // Title string prefix.
+              //     100 * 4 + // Title String size (100 chars)
+              //     4, // Author string prefix.
+              // bytes: bs58.encode(Buffer.from('<author name>')),
+              offset: 0,
+              bytes: bs58.encode(accountDiscriminator)
+          }
+      }
+    ]);
+
+    console.log('filteringBooks', filteringBooks);
+  })
 });
+
